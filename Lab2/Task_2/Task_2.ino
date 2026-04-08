b3f8f4d00852beaceeea794f844c930a8784dbb6
juulg <juulgustav@gmail.com>
2026-04-08 17:52:01 +0200

    funkar typ

--- Lab2/Task_2/Task_2.ino
+++ Lab2/Task_2/Task_2.ino
@@ -1,3 +1,6 @@
+#include <Servo.h>
+
+
 // Make sure to jump pin 2 to 13
 volatile int pulseCount = 0;
 volatile unsigned long timeSincePulse = 0;
@@ -11,18 +14,27 @@ volatile int magCount=4;
 //volatile float wheelDiameter= 0.118;
 volatile float speed=0;
 const float circumference=0.118*PI;
-float motorVoltage = 0;
+float motorThrottle = 0;
 boolean newPulse = false;
+volatile float steerAngle = 100;
+
+
+Servo steerServo;  // create servo object to control a servo
+Servo motorServo;
 void setup() {
   Serial.begin(9600);
   
-  pinMode(13, OUTPUT); // The speed to ESC
-  pinMode(12, OUTPUT); // Steer angle  
+  //pinMode(13, OUTPUT); // The speed to ESC
+  motorServo.attach(13);
+  
+  steerServo.attach(12); // Steer angle 
   pinMode(2, INPUT);   // The Interrupt pin (connected to 13)
 
   // Attach interrupt to Pin 2
   // RISING means trigger when the voltage goes from LOW to HIGH
   attachInterrupt(digitalPinToInterrupt(2), countPulse, FALLING);
+  motorServo.writeMicroseconds(1500); // neutral
+  delay(3000); // allow ESC to arm
 }
 
 void loop() {
@@ -36,17 +48,18 @@ void loop() {
   newPulse=false;
   }
   // Set speed
-  analogWrite(13, motorVoltage);
-
+  //analogWrite(13, motorThrottle);
+  motorServo.write(motorThrottle);
+  steerServo.write(steerAngle);
   recvWithEndMarker();
   if (newData == true) {
     if (receivedChars[0] == 'S') {
-       float motorVoltage = String(receivedChars).substring(1).toFloat();
-       Serial.print(motorVoltage);
-       Serial.println(" motor voltage");
+       motorThrottle = String(receivedChars).substring(1).toFloat();
+       Serial.print(motorThrottle);
+       Serial.println(" motor throttle");
       } else if (receivedChars[0] == 'A'){
-       float steerAngle = String(receivedChars).substring(1).toFloat();
-       analogWrite(12, steerAngle);
+       steerAngle = String(receivedChars).substring(1).toFloat();
+       //analogWrite(12, steerAngle);
        Serial.print(steerAngle);
        Serial.println(" angle");
       }
