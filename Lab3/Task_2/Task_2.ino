e994d231278cd6303f1a0f283b054f37bab01278
juulg <juulgustav@gmail.com>
2026-04-15 16:16:11 +0200

    Lab2 klar. Kopplat pid för lab 3

--- Lab3/Task_2/Task_2.ino
+++ Lab3/Task_2/Task_2.ino
@@ -21,6 +21,21 @@ volatile float steerAngle = 100;
 
 Servo steerServo;  // create servo object to control a servo
 Servo motorServo;
+
+
+float proportional, integral, derivative, setpoint, error = 0; // PID variables
+
+// PID parameters (example values right now)
+float Kp = 1.5;
+float Ki = 0.5;
+float Kd = 0.1;
+
+unsigned long lastTime = 0; // used in calcPID
+float previous_error = 0; // used in calcPID
+
+float throttle = 0;
+
+
 void setup() {
   Serial.begin(9600);
   
@@ -104,4 +119,35 @@ void recvWithEndMarker() {
             newData = true;
         }
     }
+}
+
+
+// use this function in the loop of arduino program
+void calcPID(float setpoint, float speed) {
+  unsigned long currentTime = millis();
+  float dt = (currentTime - lastTime) / 1000.0; // seconds
+
+  if (dt <= 0) return;
+
+  // replace with sensor reading
+  measured_speed = speed;
+
+  error = setpoint - measured_speed;
+
+  integral += error * dt;
+
+  // prevent integral windup
+  integral = constrain(integral, -100, 100);
+
+  derivative = (error - previous_error) / dt;
+
+  float output = Kp * error + Ki * integral + Kd * derivative;
+
+  output = constrain(output, 0, 255);
+
+  // apply output for next timestep
+  throttle = output;
+
+  previous_error = error;
+  lastTime = currentTime;
 }
