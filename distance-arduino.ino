4bd2fdccef8ec818b2dd8f52dde7be30a97b91f0
Kristoffer <kirlil-9@student.ltu.se>
2026-04-23 15:43:47 +0200

    added pid controller for distance

--- distance-arduino.ino
+++ distance-arduino.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 int trigL = 11; //Left
 int echoL = 3;
 int durationL;
@@ -16,9 +18,40 @@ int durationM;
 float distanceM;
 float meterM;
 
+float distancemidpoint;
+float MidpointHistory[5] = {0, 0, 0, 0, 0};
+int MidpointIndex = 0;
+
+Servo steerServo;  // create servo object to control a servo
+Servo motorServo;
+
+float proportional, integral, derivative, setpoint, error = 0; // PID variables
+
+// PID parameters (example values right now)
+float Kp = 1;
+float Ki = 3;
+float Kd = 1;
+
+unsigned long lastTime = 0; // used in calcPID
+float previous_error = 0; // used in calcPID
+
+float addMidpointAndGetAverage(float newMidpoint) {
+    MidpointHistory[MidpointIndex] = newMidpoint;
+    MidpointIndex = (MidpointIndex + 1) % 5;
+
+    float sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum += MidpointHistory[i];
+    }
+
+    return sum / 5.0;
+}
+
 void setup()
 {
   Serial.begin(9600);
+  steerServo.attach(12); // Steer angle 
+  motorServo.writeMicroseconds(1500); // neutral
 
   pinMode(trigL, OUTPUT);
   digitalWrite(trigL, LOW);
@@ -50,16 +83,19 @@ void loop()
 
   if (durationL >= 38000) {
     Serial.println("Out of range");
+    durationL = 38000;
+    distanceL = durationL * 0.034 / 2;
+    meterL = distanceL / 100;
   } 
   else {
     distanceL = durationL * 0.034 / 2;
     meterL = distanceL / 100;
 
-    Serial.print("Distance Left: ");
-    Serial.print(distanceL);
-    Serial.print(" cm\t");
-    Serial.print(meterL);
-    Serial.println(" m");
+    //Serial.print("Distance Left: ");
+    //Serial.print(distanceL);
+    //Serial.print(" cm\t");
+    //Serial.print(meterL);
+    //Serial.println(" m");
   }
 
   //for Right
@@ -70,18 +106,34 @@ void loop()
   durationR = pulseIn(echoR, HIGH);
 
   if (durationR >= 38000) {
-    Serial.println("Out of range");
+    Serial.println("Out of range Right");
+    durationR = 38000;
+    distanceR = durationR * 0.034 / 2;
+    meterR = distanceR / 100;
   } 
   else {
     distanceR = durationR * 0.034 / 2;
     meterR = distanceR / 100;
 
-    Serial.print("Distance Right: ");
-    Serial.print(distanceR);
-    Serial.print(" cm\t");
-    Serial.print(meterR);
-    Serial.println(" m");
+    //Serial.print("Distance Right: ");
+    //Serial.print(distanceR);
+    //Serial.print(" cm\t");
+    //Serial.print(meterR);
+    //Serial.println(" m");
   }
+
+  //Calculating midpoint
+distancemidpoint = (distanceR-distanceL)/2;
+
+float avgMidpoint = addMidpointAndGetAverage(distancemidpoint);
+
+Serial.print("Distance Midpoint: ");
+Serial.print(avgMidpoint);
+Serial.println(" cm\t");
+
+float setMidpoint = 0;
+
+calcPIDAngle(setMidpoint, avgMidpoint);
 
   //for Mid
   digitalWrite(trigM, HIGH);
@@ -91,19 +143,55 @@ void loop()
   durationM = pulseIn(echoM, HIGH);
 
   if (durationM >= 38000) {
-    Serial.println("Out of range");
+    Serial.println("Out of range Mid");
+    durationM = 38000;
+    distanceM = durationM * 0.034 / 2;
+    meterM = distanceM / 100;
   } 
   else {
     distanceM = durationM * 0.034 / 2;
     meterM = distanceM / 100;
 
-    Serial.print("Distance Mid: ");
-    Serial.print(distanceM);
-    Serial.print(" cm\t");
-    Serial.print(meterM);
-    Serial.println(" m");
+    //Serial.print("Distance Mid: ");
+    //Serial.print(distanceM);
+    //Serial.println(" cm\t");
   }
 
-  delay(500);
+  delay(100);
+}
+
+// use this function in the loop of arduino program
+void calcPIDAngle(float setmidpoint, float midpoint) {
+  unsigned long currentTime = millis();
+  float dt = (currentTime - lastTime) / 1000.0; // seconds
+
+  if (dt <= 0) return;
+
+  // replace with sensor reading
+  float measured_midpoint = midpoint;
+
+  float error = setmidpoint - measured_midpoint;
+
+  integral += error * dt;
+
+  // prevent integral windup
+  integral = constrain(integral, -100, 100);
+ // Serial.print(integral);
+ // Serial.println(" integral");
+  derivative = (error - previous_error) / dt;
+
+  //float output = Kp * error + Ki * integral + Kd * derivative;
+  float output = Kp * error;
+
+
+  Serial.print(output);
+  Serial.println(" wanted distance");
+  output = constrain(output, 55, 255);
+
+  // apply output for next timestep
+ // throttle = output;
+
+  previous_error = error;
+  lastTime = currentTime;
 }
  
